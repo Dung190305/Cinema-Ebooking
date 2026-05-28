8fbefabf8d6671eba8fb0978e5c649739a9eda6f
Dung190305 <nguoigay102@gmai.coms>
2026-05-29 03:22:28 +0700

    feature(Refund): thêm chức năng cho dto

---
+++ backend/src/main/java/com/cinemaebooking/backend/refund/application/dto/ProcessRefundRequest.java
@@ -0,0 +1,13 @@
+package com.cinemaebooking.backend.refund.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProcessRefundRequest {
+
+    private String adminNote;
+}
